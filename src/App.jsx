26fa6671117b7f5a64e8ca1ec808fee44f67d909
tatang <itsme@tatang.dev>
2023-03-12 00:55:21 +0700

    react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
+import { IconBrandFacebook, IconBrandTwitter } from '@tabler/icons-react';
+import clsx from 'clsx';
+
 export default function App() {
     return (
-        <div>
-            <h1 className={'text-xl'}>Hello world</h1>
+        <div className={'bg-slate-900 grid place-content-center min-h-screen'}>
+            <div className='flex gap-x-2'>
+                <Button
+                    {...{
+                        // type: 'button',
+                        text: 'Login',
+                        onClick: () => console.log('Click with another style'),
+                    }}
+                />
+                <Button onClick={() => console.log('Register')} type='button'>
+                    <IconBrandFacebook />
+                    Register
+                </Button>
+                <Button onClick={() => console.log('Login')} type='submit'>
+                    <IconBrandTwitter />
+                    Login
+                </Button>
+            </div>
         </div>
+    );
+}
+
+function Button(props) {
+    const {
+        className = 'bg-blue-600',
+        text,
+        children,
+        type = 'submit',
+    } = props;
+    return (
+        <button
+            {...props}
+            type={type}
+            className={clsx(
+                className,
+                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex item-center gap-x-2 text-white px-4 py-2 rounded'
+            )}
+        >
+            {text || children}
+        </button>
     );
 }
